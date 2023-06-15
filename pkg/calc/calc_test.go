ad38b541fe1f33b94b47e039c915e0ac33d5d766
anwardh <anwar@digitalhouse.com>
2023-06-15 16:13:58 -0300

    add. testes para o If em Subtração e Panic em Divisão

--- pkg/calc/calc_test.go
+++ pkg/calc/calc_test.go
@@ -4,22 +4,44 @@ import (
 	"testing"
 
 	"github.com/anwardh/gotestes/pkg/calc"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSoma(t *testing.T) {
 	resultadoAtual := calc.Soma(5, 5)
 	resultadoEsperado := 10
 
-	if resultadoAtual != resultadoEsperado {
-		t.Errorf("failed: o resultado atual %v é diferente do resultado esperado %v", resultadoAtual, resultadoEsperado)
-	}
+	// if resultadoAtual != resultadoEsperado {
+	// 	t.Errorf("failed: o resultado atual %v é diferente do resultado esperado %v", resultadoAtual, resultadoEsperado)
+	// }
+
+	assert.Equal(t, resultadoEsperado, resultadoAtual, "o resultado atual precisa ser igual ao resultado esperado")
 }
 
 func TestSubtracao(t *testing.T) {
+	resultadoAtual := calc.Subtracao(5, 3)
+	resultadoEsperado := 0
+
+	// if resultadoAtual != resultadoEsperado {
+	// 	t.Errorf("failed: o resultado atual %v é diferente do resultado esperado %v", resultadoAtual, resultadoEsperado)
+	// }
+
+	assert.Equal(t, resultadoEsperado, resultadoAtual, "o resultado atual precisa ser igual ao resultado esperado")
+}
+
+func TestSubtracao2(t *testing.T) {
 	resultadoAtual := calc.Subtracao(5, 5)
 	resultadoEsperado := 0
 
-	if resultadoAtual != resultadoEsperado {
-		t.Errorf("failed: o resultado atual %v é diferente do resultado esperado %v", resultadoAtual, resultadoEsperado)
-	}
+	// if resultadoAtual != resultadoEsperado {
+	// 	t.Errorf("failed: o resultado atual %v é diferente do resultado esperado %v", resultadoAtual, resultadoEsperado)
+	// }
+
+	assert.Equal(t, resultadoEsperado, resultadoAtual, "o resultado atual precisa ser igual ao resultado esperado")
+}
+
+func TestDivisao(t *testing.T) {
+	resultadoAtual := calc.Divisao(5, 5)
+
+	assert.NotNil(t, resultadoAtual)
 }
